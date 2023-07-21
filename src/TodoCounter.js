d4155f430fed83e153af087728a6b12d58a99130
Juan Telis <Juantelis03@gmail.com>
2023-07-21 13:18:41 -0300

    NotCompletedTodo, deleteTodo

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,10 +1,24 @@
 import './TodoCounter.css'
 
 function TodoCounter({cantidad, total}) {
-    return (
-      <h1 className='TodoCounter'>
-        Completaste <span>{cantidad}</span> de  <span>{total}</span> materias
-      </h1>
+  const validator = (cantidad, total) => {
+    if (cantidad !== total) {
+      return (
+        <h1 className='TodoCounter'>
+          Completaste <span>{cantidad}</span> de  <span>{total}</span> materias
+        </h1>
+      )
+    } else {
+      (
+        <h1 className='TodoCounter'>
+          Completaste <span>{cantidad}</span> de  <span>{total}</span> materias
+        </h1>
+      )
+    }
+  }
+    
+  return (   
+      validator(cantidad, total)         
     )
 }
 
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,18 @@
 import './TodoItem.css'
 
-function TodoItem(props) {
+function TodoItem(props) {    
     return (
       <li className='TodoItem'>
-        <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>
+        <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={props.onComplete}        
+        >
           V {props.completed}
         </span>
         <p className={`TodoItem-p' ${props.completed && "TodoItem-p--complete"}`}>
           {props.text}
         </p>
-        <span className='Icon Icon-delete'>
-          X
+        <span className='Icon Icon-delete' onClick={props.notCompleted}>
+          X 
         </span>
       </li>
     )
