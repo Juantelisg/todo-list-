ff0c14e30bd0ea55ca66fc33e0afedce5d20affa
Juan Telis <Juantelis03@gmail.com>
2023-07-21 13:23:37 -0300

    x

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,14 @@ import './TodoItem.css'
 function TodoItem(props) {
     return (
       <li className='TodoItem'>
-        <span className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}>
+        <span 
+        className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={() => {
+          document.className="Icon-check--active"
+          console.log("n")
+        }
+        } 
+        >
           V {props.completed}
         </span>
         <p className={`TodoItem-p' ${props.completed && "TodoItem-p--complete"}`}>
